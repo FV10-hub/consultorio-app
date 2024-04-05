99a84d82851cf177ca2293676e8e18d47d310866
Fernando Vazquez <fernandorafa@live.com>
2024-04-05 17:32:34 -0400

    resaltar el menu atual

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,12 +1,46 @@
-import { menus } from "@/prisma/data/menus";
+"use client";
 import ContentMenu from "../ui/ContentMenu";
+import { useParams } from "next/navigation";
 
-async function getMenus() {
-  return await menus;
+function getMenus() {
+  return [
+    {
+      id: 1,
+      name: "Personas",
+      imagen: "persona",
+      path: "personas",
+    },
+    {
+      id: 2,
+      name: "Pacientes",
+      imagen: "paciente",
+      path: "pacientes",
+    },
+    {
+      id: 3,
+      name: "Doctores",
+      imagen: "doctor",
+      path: "doctores",
+    },
+    {
+      id: 4,
+      name: "Especialidades",
+      imagen: "especialidad",
+      path: "especialidades",
+    },
+    {
+      id: 5,
+      name: "Fichas",
+      imagen: "ficha",
+      path: "fichas",
+    },
+  ];
 }
 
-export default async function Sidebar() {
-  const menus = await getMenus();
+export default function Sidebar() {
+  const menus = getMenus();
+  const parametro = useParams<{ menu: string }>();
+  console.log(parametro);
   return (
     <aside className="md:w-72 md:h-screen bg-white">
       <nav className="mt-10">
--- components/ui/ContentMenu.tsx
+++ components/ui/ContentMenu.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 type menuProps = {
   menu: {
@@ -11,9 +13,11 @@ type menuProps = {
 };
 
 export default function ContentMenu({ menu }: menuProps) {
+  const pathname = usePathname();
+  console.log(pathname)
   return (
     <div
-      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+      className={`${menu.path === pathname.replace("/", "") ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
       <div className="w-16 h-16 relative">
         <Image fill src={`${menu.imagen}.svg`} alt="Imagen" />
